bc487f075ccbfef3c5a995fcf075780ba2ed465d
dduc8442-cpu <dduc8442@gmail.com>
2026-02-07 09:51:34 +0700

    commit

--- bai2.c
+++ bai2.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [ĐẶNG ĐÌNH ĐỨC]
+ * MSSV:      [PS48675]
+ * Lớp:       [CS21302]
  *****************************************************************************/
 
 //  BÀI 2: TÌM GIÁ TRỊ LỚN NHẤT VÀ NHỎ NHẤT TRONG MẢNG 
@@ -9,4 +9,35 @@
 //  Output: Xuất ra màn hình giá trị nhỏ nhất và lớn nhất của mảng
 
 // VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+int main() {
+    int n;
+    int i;
 
+    printf("Nhap so phan tu n: ");
+    scanf("%d", &n);
+
+    int mang[n];
+
+    for (i = 0; i < n; i++) {
+        printf("Nhap mang[%d]: ", i);
+        scanf("%d", &mang[i]);
+    }
+
+    int max = mang[0];
+    int min = mang[0];
+
+    for (i = 1; i < n; i++) {
+        if (mang[i] > max) {
+            max = mang[i];
+        }
+        if (mang[i] < min) {
+            min = mang[i];
+        }
+    }
+
+    printf("Gia tri lon nhat: %d\n", max);
+    printf("Gia tri nho nhat: %d\n", min);
+
+    return 0;
+}
